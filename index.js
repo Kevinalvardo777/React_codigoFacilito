9eba6a08c48b2c7cad389c686557943cd73b4554
Kevinalvardo777 <kevinalva_777@hotmail.com>
2020-04-24 01:24:09 -0500

    2da practica

--- index.js
+++ index.js
@@ -1,63 +1,40 @@
-import React from "react";
-import { render } from "react-dom";
+import React, { useState } from 'react';
+import {render} from 'react-dom';
 
-function SaludarEnIdiomas({ idioma }) {
-  if (idioma === "en") {
-    return <span>Hello</span>;
-  } else {
-    return <span>Hola</span>;
+//const App = () => <h1>Hola mundo todos</h1>
+
+/*class App extends Component {
+  render(){
+    return <h1>Hola mundo</h1>
   }
-}
-const Saludo = () => {
-  const nombre = "Uriel";
-  return (
-    <p>
-      <SaludarEnIdiomas idioma="es" /> {`${nombre}`}
-    </p>
-  );
-};
+}*/
 
+/*const nombres = [
+  'Uriel',
+  'Fernando',
+  'Marines',
+  'Cany'
+]
 
-const Saludo2 = () => {
-  const nombre = "Uriel";
-  const idioma = "es";
-  return <div>{idioma === "en" ? <p>Hello</p> : <p>Hola</p>}</div>;
-};
-
-const nombres = ["Uriel", "Edu", "Joel", "Tai"];
-
-function getNombres() {
-  const elementosLista = [];
-  for (var i = 0; i <= nombres.length; i++) {
-    elementosLista.push(<li>{nombres[i]}</li>);
-  }
-  return elementosLista;
+const Saludar = ({nombre, idioma}) => {
+  const saludo = idioma === 'es' ? 'Hola' : 'Hello'
+  return <h1>{saludo} {nombre}</h1>
 }
 
-/*cuando queremos imprimir una lista, es necesario incluir una key
-para que no tengamos que estar renderizando todo el documento
-si no solo ese elemento que en caso hayamos cambiado*/
-
-const Nombres = () => {
-  /*return (
-    <ul> {getNombres()}</ul>
-  )*/
+Saludar.defaultProps = {
+  idioma: 'en'
+}*/
+const Button = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <ul>
-      {" "}
-      {nombres.map((nombre, index) => (
-        <li key={index}>{nombre}</li>
-      ))}
-    </ul>
-  );
-};
+    <div>
+      <p>Presionado : {counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Click me</button>
+    </div>
+  )
+}
 
-const App = () => {
-  return (
-    <h1>
-      <Nombres />
-    </h1>
-  );
-};
-
-render(<App />, document.getElementById("react-app"));
+const App = () =>{
+  return <div><Button/></div>
+}
+render (<App />, document.getElementById('react-app'));
