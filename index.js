12f5fb7217be3c31eb7ee8004298c284c358a9dd
Kevinalvardo777 <kevinalva_777@hotmail.com>
2020-04-25 19:50:03 -0500

    3 commit practicas

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-import React, { useState } from 'react';
-import {render} from 'react-dom';
+import React, {useState} from 'react'
+import {render} from 'react-dom'
 
-//const App = () => <h1>Hola mundo todos</h1>
-
-/*class App extends Component {
-  render(){
-    return <h1>Hola mundo</h1>
-  }
-}*/
-
-/*const nombres = [
-  'Uriel',
-  'Fernando',
-  'Marines',
-  'Cany'
-]
-
-const Saludar = ({nombre, idioma}) => {
-  const saludo = idioma === 'es' ? 'Hola' : 'Hello'
-  return <h1>{saludo} {nombre}</h1>
-}
-
-Saludar.defaultProps = {
-  idioma: 'en'
-}*/
-const Button = () => {
-  const [counter, setCounter] = useState(0);
-  return (
+const Saludo =() => {
+  const [name, setName] = useState("");
+  return(
+    /* SyntheticEvent */
     <div>
-      <p>Presionado : {counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>Click me</button>
+      <input type="text" onChange = { (ev) => console.log(ev)}/>
+      <p>Hola {name}</p>
+      <a href="#" onClick ={(ev) => ev.preventDefault()}></a>
     </div>
-  )
+  );
 }
 
-const App = () =>{
-  return <div><Button/></div>
+const App =() => {
+  return <div><Saludo /></div>
 }
-render (<App />, document.getElementById('react-app'));
+
+render(<App/>, document.getElementById("react-app"))
