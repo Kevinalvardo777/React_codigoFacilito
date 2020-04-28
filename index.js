9e681470f86cb2db20ac1c26f56c3421dfa2ba5f
Kevinalvardo777 <kevinalva_777@hotmail.com>
2020-04-28 17:47:29 -0500

    Props

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {render} from 'react-dom'
+import PropTypes from 'prop-types'
 
+class App extends React.Component {
 
-
-
-
-const Form =()=> {
-  let [title, setTitle] = useState("");
-  let [body, setBody] = useState("");
-
-  const sendForm = (ev) =>{
-  ev.preventDefault()
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-  method : 'POST', 
-  body: JSON.stringify({
-    title : title,
-    body: body, 
-    userId: 1
-  }), 
-  headers: {
-    "Content-type" : "application/json; charset=UFT-8"
+  static defaultProps = {
+    name: "Cody Default"
   }
-})
-  .then(response => response.json())
-  .then(json => {
-     setTitle("");
-      setBody("");
-      console.log(json) 
-    
-  })
-   
-}
-  return( 
-    <form onSubmit = {(ev)=> sendForm(ev)}>
-      <div>
-        <label htmlFor="title">Titulo</label>
-        <input type="text" value={title} id="title" onChange={ (ev) => setTitle(ev.target.value)}/>
-      </div>
-      <div>
-        <label htmlFor="body">Publicacion</label>
-        <textarea id="body" value={body} onChange={ (ev)=>
-        setBody(ev.target.value)}></textarea>
-      </div>
-      <input type="submit" value="Enviar"/>
-    </form>
-  )
+
+  static propTypes = {
+    name: PropTypes.string
+  }
+  render(){
+    console.log(this.props)
+    return <p>Hola mundo</p>
+  }
 }
 
-
-const App =() => {
-  return <div><Form /></div>
-}
-
-render(<App/>, document.getElementById("react-app"))
+render(<App name={2}/>, document.getElementById("react-app"))
